7f669a1ea5308c35a0c2f806879d7c49ccf99313
Alja Prah <alja.prah@ijs.si>
2024-12-03 19:32:08 +0100

    made navbar, sections, contact form and map

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      document.querySelector(link.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });  
